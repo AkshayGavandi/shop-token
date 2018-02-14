2974a918db411b9ba82c5bb451d2c0d928fdac91
Pavel Kiselyov <pavel.kiselyov@protonmail.com>
2018-02-14 08:26:32 +0700

    Set test offering & bonus values (#39)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,12 @@ var ShopToken = artifacts.require("ShopToken");
 module.exports = function (deployer, network, accounts) {
   // Mint 1B tokens, transfer 100K for dutch auction
   const multiplier = 10 ** 18;
+
+  // Token constructor parameters
   const initialSupply = (10 ** 9) * multiplier;
-  const auctionSupply = (10 ** 5) * multiplier;
+  const offering = 10000;
+  const bonus = 500;
+  const auctionSupply = offering + bonus;
 
   // Start with 500 Wei price per token unit
   const priceStart = 500;
